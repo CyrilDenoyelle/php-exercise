1bb051c283ca56c7ad26c6d8b96c4d47504ea618
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 14:04:10 +0100

    function exercice 8

---
+++ function/exercice8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function</title>
+</head>
+<body>
+	<?php
+	echo "Exercice 8 <br />";
+	function tripladd($nb1 = 1, $nb2 = 2, $nb3 = 3){
+		return $nb1 + $nb2 + $nb3;
+	};
+	echo tripladd()
+	?>
+</body>
+</html>
