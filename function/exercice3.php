4e88f1aad0113dc8dec6f7381b1a2472c471c3ab
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 13:21:06 +0100

    function exercice 3

---
+++ function/exercice3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function</title>
+</head>
+<body>
+	<?php
+	echo "Exercice 3 <br />";
+	function returnstring($string1, $string2){
+		return $string1 . $string2;
+	};
+	echo returnstring("coucou", "string");
+	?>
+</body>
+</html>
