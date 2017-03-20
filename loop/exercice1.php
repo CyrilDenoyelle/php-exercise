7df5bfe9a298fd9029f3373ab68d3ab25af799d2
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 12:11:18 +0100

    loop exercice1

---
+++ loop/exercice1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<?php
+		echo "Exercice 1 <br />";
+		$i = 0;
+		while ($i < 10) {
+			echo $i++;
+		}
+		?>
+</body>
+</html>
