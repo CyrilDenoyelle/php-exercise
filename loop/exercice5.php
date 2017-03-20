1780edb79935ad3360b1ab221452d16d077444a5
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 12:13:24 +0100

    exercice 5

---
+++ loop/exercice5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<?php
+		echo "<br /> Exercice 5 <br />";
+
+		for ($i = 0; $i <= 15; $i += 1) {
+			echo $i . "On y arrive presque <br />";
+		}
+		?>
+</body>
+</html>
