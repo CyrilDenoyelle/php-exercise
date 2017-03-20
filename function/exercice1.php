cc329dc1ac8be8107094340726a4ae7cceba15c3
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 12:25:01 +0100

    function exercice 1

---
+++ function/exercice1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function</title>
+</head>
+<body>
+	<?php
+	echo "Exercice 1 <br />";
+	function truereturn(){
+		return true;
+	};
+	echo truereturn();
+	?>
+</body>
+</html>
