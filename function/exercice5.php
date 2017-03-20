13c4db237d51d28c837bd81aa37952b52d1b61bc
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 13:33:22 +0100

    function exercice 5

---
+++ function/exercice5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function</title>
+</head>
+<body>
+	<?php
+	echo "Exercice 5 <br />";
+	function concatnbstring($nb, $string){
+		return $nb . $string;
+	};
+	echo concatnbstring(5, "coucou")
+	?>
+</body>
+</html>
