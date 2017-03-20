ff166c62c22f962dbf8939da10094bb3637b57e0
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 12:18:17 +0100

    exercice 8

---
+++ loop/exercice8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<?php
+		echo "<br /> Exercice 8 <br />";
+
+		for ($i = 200; $i >= 0; $i -= 12) {
+			echo $i . "Enfin !!!! <br />";
+		}
+		?>
+</body>
+</html>
