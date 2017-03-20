bea2c435e2eb2869fa1b5152b2499688813508ab
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 12:15:06 +0100

    exercice 7

---
+++ loop/exercice7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+		<?php
+		echo "<br /> Exercice 7 <br />";
+
+		for ($i = 1; $i <= 100; $i += 15) {
+			echo $i . "On tient le bon bout <br />";
+		}
+		?>
+</body>
+</html>
