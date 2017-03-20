8feb22c9b50e2329c24a72f1fa7943d98dc216cc
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-20 16:11:25 +0100

    array exercice 5 a 7

---
+++ array/exercice5_7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array</title>
+</head>
+<body>
+	<?php 
+	echo "Exercice 5 <br />";
+	$hautdefr = ["02" => "Aisne", "59" => "Nord" , "60" => "Oise" , "62" => "Pas-de-Calais", "80" => "Somme"];
+	echo "<hr>Exercice 6 <br />";
+	echo $hautdefr["59"];
+	echo "<hr>Exercice 7 <br />";
+	$hautdefr["51"] = "Marne";
+	?>
+</body>
+</html>
